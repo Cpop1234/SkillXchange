7005a1f7524d0dbf60cacf0c03c553189cfc169d
Patil <patil@example.com>
2026-02-22 15:30:32 +0530

    Replace 'Skill Swap' with 'SkillXchange' on landing page

--- Frontend/src/Pages/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -102,15 +102,15 @@ const LandingPage = () => {
         <div>
           <img src={"/assets/images/1.png"} alt="Above Image" style={imageStyle} />
           <div style={titleContainerStyle}>
-            <h1 style={titleStyle}>SKILL SWAP</h1>
+            <h1 style={titleStyle}>SkillXchange</h1>
           </div>
           <img src={"/assets/images/2.png"} alt="Below Image" style={imageBelowStyle} />
         </div>
       </div>
-      <h2 style={contentTitleStyle}>WHY SKILL SWAP?</h2>
-      <div id="why-skill-swap" style={textContainer}>
+      <h2 style={contentTitleStyle}>WHY SKILLXCHANGE?</h2>
+      <div id="why-skillxchange" style={textContainer}>
         <div style={descriptionStyle}>
-          At Skill Swap, we believe in the power of mutual learning and collaboration. Here's why Skill Swap is the
+          At SkillXchange, we believe in the power of mutual learning and collaboration. Here's why SkillXchange is the
           ultimate platform for skill acquisition and knowledge exchange:
           <br />
           <br />
@@ -122,22 +122,22 @@ const LandingPage = () => {
           <br />
           <br />
           <h4 style={{ color: "#028477" }}>➋ Share Your Expertise:</h4> Have a skill or passion you're eager to share?
-          Skill Swap provides a platform for you to become a mentor yourself. Share your expertise with others, foster a
+          SkillXchange provides a platform for you to become a mentor yourself. Share your expertise with others, foster a
           sense of community, and contribute to the growth of aspiring learners.
           <br />
           <br />
           <h4 style={{ color: "#028477" }}>➌ Collaborative Environment:</h4> Our community thrives on collaboration.
           Connect with like-minded individuals, participate in group projects, and engage in discussions that fuel
-          creativity and innovation. Skill Swap isn't just about individual growth—it's about collective advancement.
+          creativity and innovation. SkillXchange isn't just about individual growth—it's about collective advancement.
           <br />
           <br />
-          <h4 style={{ color: "#028477" }}>➍ Diverse Learning Opportunities:</h4> With Skill Swap, the possibilities are
+          <h4 style={{ color: "#028477" }}>➍ Diverse Learning Opportunities:</h4> With SkillXchange, the possibilities are
           endless and <b>free of cost</b>. Explore a wide range of topics and disciplines, from traditional crafts to
           cutting-edge technologies. Our diverse library of skills ensures there's something for everyone, regardless of
           your interests or background.
           <br />
           <br />
-          <h4 style={{ color: "#028477" }}>➎ Continuous Growth:</h4> Learning is a lifelong journey, and Skill Swap is
+          <h4 style={{ color: "#028477" }}>➎ Continuous Growth:</h4> Learning is a lifelong journey, and SkillXchange is
           here to support you every step of the way. Whether you're a novice or a seasoned professional, our platform
           empowers you to continuously expand your knowledge, challenge yourself, and embrace new opportunities.
         </div>
